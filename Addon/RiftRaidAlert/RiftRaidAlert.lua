dd00dae95b5835eef554cc3819201a0b4a9a597b
Bamux <bamueq@googlemail.com>
2016-05-10 01:54:30 +0200

    Update RiftRaidAlert.lua

--- Addon/RiftRaidAlert/RiftRaidAlert.lua
+++ Addon/RiftRaidAlert/RiftRaidAlert.lua
@@ -1,41 +1,5 @@
-﻿local function Pagura_Golems_Target_Name()
-    local Crustok = Inspect.Unit.Castbar("u2B75B2CB73ED8E64")
-    if Crustok ~= nil then
-        if Crustok.abilityName == "Pain Bringer" then
-            local Crustok_target = Inspect.Unit.Detail("u2B75B2CB73ED8E64.target")
-            if Crustok_target ~= nil then
-                print ("Crustok -> target -> "..Crustok_target.name)
-            end
-        end
-    end
-    local Brachy = Inspect.Unit.Castbar("u50D54CD171D9D26A")
-    if Brachy ~= nil then
-        if Brachy.abilityName == "Pain Bringer" then
-            local Brachy_target = Inspect.Unit.Detail("u50D54CD171D9D26A.target")
-            if Brachy_target ~= nil then
-                print ("Brachy -> target -> "..Brachy_target.name)
-            end
-        end
-    end
-end
 
-
-local function getAbilityName(event, units)
-    if units then
-        for id,value in pairs(units) do
-            local details = Inspect.Unit.Detail(id)
-            local cast = Inspect.Unit.Castbar(id)
-            if value then
-                if details.player == nil then
-                    print(details.name.." -> "..cast.abilityName)
-                end
-            end
-        end
-    end
-    Pagura_Golems_Target_Name()
-end
-
-
+-- print all removed Debuffs (Player <-  remove Debuff)  and Buffs (NPC <- remove Buff)
 local function getRemoveBuffName(event, unit, buffs)
     if unit then
         local details = Inspect.Unit.Detail(unit)
@@ -43,8 +7,8 @@ local function getRemoveBuffName(event, unit, buffs)
             local buff = Inspect.Buff.Detail(unit, buffid)
             for key, value in pairs(rra_bufflist) do
                 if value.id == buffid then
-                    print(value.name.." -> remove -> "..details.name)
-                    table.remove(rra_bufflist,key)
+                    print(details.name .. " <- remove " .. value.name )
+                    table.remove(rra_bufflist, key)
                 end
             end
         end
@@ -52,24 +16,51 @@ local function getRemoveBuffName(event, unit, buffs)
 end
 
 
+-- print Debuffs on all Player's (Player <-  NPC) and Buffs on all NPC's (NPC <- Buff)
 local function getAddBuffName(event, unit, buffs)
     if unit then
         local details = Inspect.Unit.Detail(unit)
         for buffid, typeid in pairs(buffs) do
             local buff = Inspect.Buff.Detail(unit, buffid)
-            if details.player then
+            local target = Inspect.Unit.Detail(buff.caster)
+            if details.player ~= nil then
                 if details.id ~= buff.caster then
-                    if buff.curse or buff.debuff or buff.disase then
-                        print(buff.name.." -> "..details.name)
-                        local bufflist = {id=buffid, name=buff.name}
-                        table.insert (rra_bufflist,bufflist)
+                    if target ~= nil then
+                        if target.player == nil then
+                            if buff.curse or buff.debuff or buff.disase then
+                                print(details.name .. " <- " ..buff.name)
+                                local bufflist = { id = buffid, name = buff.name }
+                                table.insert(rra_bufflist, bufflist)
+                            end
+                        end
                     end
                 end
             else
-                if details.id == buff.caster then
-                    print(buff.name.." -> "..details.name)
-                    local bufflist = {id=buffid, name=buff.name}
-                    table.insert (rra_bufflist,bufflist)
+                if details.id == buff.caster and details.relation == "hostile" then
+                    print(details.name .. " <- " ..buff.name)
+                    local bufflist = { id = buffid, name = buff.name }
+                    table.insert(rra_bufflist, bufflist)
+                end
+            end
+        end
+    end
+end
+
+
+-- print all Abilities (with cast time) from NPC's and show their target (NPC -> Ability -> Target)
+local function getAbilityName(event, units)
+    if units then
+        for id, value in pairs(units) do
+            if value then
+                local details = Inspect.Unit.Detail(id)
+                local cast = Inspect.Unit.Castbar(id)
+                if details.relation == "hostile" then
+                    local target = Inspect.Unit.Detail(id..".target")
+                    if target ~= nil then
+                        print (details.name .. " -> " .. cast.abilityName.. " -> " .. target.name)
+                    else
+                        print(details.name .. " -> " .. cast.abilityName)
+                    end
                 end
             end
         end
@@ -80,50 +71,60 @@ end
 local function CombatBegin()
     local details = Inspect.Unit.Detail("player.target")
     if details ~= nil then
-        print ("Combat Begin -> "..details.name)
+        print("Combat Begin -> " .. details.name)
     else
-        print ("Combat Begin")
+        print("Combat Begin")
     end
 end
 
-local function CombatEnd()
-    print ("Combat End")
-end
 
-local function rra_start()
-    print ("Rift Raid Alert started")
-    Command.Event.Attach(Event.Unit.Castbar, getAbilityName, "AbilityName")
-    Command.Event.Attach(Event.Buff.Remove, getRemoveBuffName, "buffremoveevent")
-    Command.Event.Attach(Event.Buff.Add, getAddBuffName, "buffaddevent")
-    Command.Event.Attach(Event.System.Secure.Enter, CombatBegin,"Combat Begin")
-    Command.Event.Attach(Event.System.Secure.Leave, CombatEnd,"Combat End")
+local function CombatEnd()
+    print("Combat End")
+    local count = #rra_bufflist
+    for i=0, count do rra_bufflist[i]=nil end
 end
 
 
 local function rra_stop()
-    print ("Rift Raid Alert stoped")
-    Command.Event.Detach(Event.Unit.Castbar, getAbilityName, "AbilityName")
+	Command.Event.Detach(Event.Buff.Remove, getRemoveBuffName, "buffremoveevent")
+	Command.Event.Detach(Event.Buff.Add, getAddBuffName, "buffaddevent")
+	Command.Event.Detach(Event.Unit.Castbar, getAbilityName, "AbilityName")
+	Command.Event.Detach(Event.System.Secure.Enter, CombatBegin, "CombatBegin")
+	Command.Event.Detach(Event.System.Secure.Leave, CombatEnd, "CombatEnd")
 end
 
 
-function slashHandler(h, args)
-    local r = {}
-    local numargs = 0
-    for token in string.gmatch(args, "[^%s]+") do
-        r[numargs] = token
-        numargs=numargs+1
-    end
-    if r[0] == "start" then
-        rra_start()
-        return
-    end
-    if r[0] == "stop" then
-        rra_stop()
-        return
-    end
-    print ("/rra start - Rift Raid Alert start")
-    print ("/rra stop - Rift Raid Alert stop")
+local function rra_start()
+	rra_stop()
+	Command.Event.Attach(Event.Buff.Remove, getRemoveBuffName, "buffremoveevent")
+	Command.Event.Attach(Event.Buff.Add, getAddBuffName, "buffaddevent")
+	Command.Event.Attach(Event.Unit.Castbar, getAbilityName, "AbilityName")
+	Command.Event.Attach(Event.System.Secure.Enter, CombatBegin, "CombatBegin")
+	Command.Event.Attach(Event.System.Secure.Leave, CombatEnd, "CombatEnd")
 end
+
+
+local function slashHandler(h, args)
+	local r = {}
+	local numargs = 0
+	for token in string.gmatch(args, "[^%s]+") do
+		r[numargs] = token
+		numargs = numargs + 1
+	end
+	if r[0] == "start" then
+		print("Rift Raid Alert started")
+		rra_start()
+		return
+	end
+	if r[0] == "stop" then
+		print("Rift Raid Alert stoped")
+		rra_stop()
+		return
+	end
+	print("/rra start - Rift Raid Alert start")
+	print("/rra stop - Rift Raid Alert stop")
+end
+
 
 rra_bufflist = {}
 print("/rra - for a list of commands")
