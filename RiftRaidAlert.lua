d7d5f01778797ea0c61fed78052169d7939be531
Bamux <bamueq@googlemail.com>
2016-06-30 15:32:25 +0200

    save /rra start or /rra stop status now you dont want to use /rra start after each restart

--- RiftRaidAlert.lua
+++ RiftRaidAlert.lua
@@ -1,29 +1,45 @@
-﻿local function CombatBegin()
-    if rra_combatbegin == "out_of_combat" then
-        local inspect = Inspect.Unit.Detail
-        local list = Inspect.Unit.List()
-        local format = tostring
-        for k,v in pairs(list) do
-            local detail = inspect(format(k))
-            if detail and detail.relation == "hostile" and detail.healthMax > 20000000 and detail.health > detail.healthMax*0.98 then
+﻿local rra_boss_id
+local rra_bufflist = {}
+
+
+local function CombatEnd()
+    if rra_boss_id then
+        print("Combat End")
+        local count = #rra_bufflist
+        for i=0, count do rra_bufflist[i]=nil end
+        rra_boss_id = nil
+    end
+end
+
+
+local function CombatCheck(event, units) -- Check Combat Status (Combat Begin, Combat End)
+    if not rra_boss_id then
+        local unit_details = Inspect.Unit.Detail(units)
+        for id, detail in pairs(unit_details) do
+            if detail.relation == "hostile" and detail.healthMax > 20000000 and detail.health > detail.healthMax*0.98 then
                 print("Combat Begin -> ".. detail.name)
-                rra_combatbegin = detail.id
+                rra_boss_id = detail.id
+                break
             end
         end
     end
 end
 
 
-local function CombatEnd()
-    if rra_combatbegin ~= "out_of_combat" then
-        print("Combat End")
-        local count = #rra_bufflist
-        for i=0, count do rra_bufflist[i]=nil end
-        rra_combatbegin = "out_of_combat"
-        rra_health = 0
+local function CombatDeath(event, units)
+    if rra_boss_id then
+        local unit_details = Inspect.Unit.Detail(units)
+        for id, detail in pairs(unit_details) do
+            if detail.id == rra_boss_id then
+                print("Combat End -> ".. detail.name)
+                local count = #rra_bufflist
+                for i=0, count do rra_bufflist[i]=nil end
+                rra_boss_id = nil
+                break
+            end
+        end
     end
 end
-
 
 -- print Debuffs on all Player's (Player <-  NPC) and Buffs on all NPC's (NPC <- Buff)
 local function getAddBuffName(event, unit, buffs)
@@ -111,93 +127,33 @@ local function getAbilityName(event, units)
 end
 
 
--- print the Hitpoints from the first NPC with agro
-local function CheckHP(units)
-    if rra_combatbegin ~= "out_of_combat" then
-        local detail = Inspect.Unit.Detail(rra_combatbegin)
-        if detail ~= nil then
-            local percent = (100/detail.healthMax*detail.health)
-            if detail.health == detail.healthMax then
-                if rra_health ~= 100 then
-                    print(detail.name .. " -> " .. "100 %")
-                    rra_health = 100
-                end
-            elseif percent <= 90 and  percent > 80 then
-                if rra_health ~= 90 then
-                    print(detail.name .. " -> " .. "90 %")
-                    rra_health = 90
-                end
-            elseif percent <= 80 and  percent > 70 then
-                if rra_health ~= 80 then
-                    print(detail.name .. " -> " .. "80 %")
-                    rra_health = 80
-                end
-            elseif percent <= 70 and  percent > 60 then
-                if rra_health ~= 70 then
-                    print(detail.name .. " -> " .. "70 %")
-                    rra_health = 70
-                end
-            elseif percent <= 60 and  percent > 50 then
-                if rra_health ~= 60 then
-                    print(detail.name .. " -> " .. "60 %")
-                    rra_health = 60
-                end
-            elseif percent <= 50 and  percent > 40 then
-                if rra_health ~= 50 then
-                    print(detail.name .. " -> " .. "50 %")
-                    rra_health = 50
-                end
-            elseif percent <= 40 and  percent > 30 then
-                if rra_health ~= 40 then
-                    print(detail.name .. " -> " .. "40 %")
-                end
-                rra_health = 40
-            elseif percent <= 30 and  percent > 20 then
-                if rra_health ~= 30 then
-                    print(detail.name .. " -> " .. "30 %")
-                end
-                rra_health = 30
-            elseif percent <= 20 and  percent > 10 then
-                if rra_health ~= 20 then
-                    print(detail.name .. " -> " .. "20 %")
-                end
-                rra_health = 20
-            elseif percent <= 10 and  percent > 0 then
-                if rra_health ~= 10 then
-                    print(detail.name .. " -> " .. "10 %")
-                end
-                rra_health = 10
-            elseif detail.health <= 1 then
-                if rra_health ~= 0 then
-                    print(detail.name .. " -> " .. "0 %")
-                    CombatEnd()
-                end
-            end
-        end
-    end
-end
-
-
 local function rra_stop()
-    Command.Event.Attach(Event.Unit.Detail.Combat, CombatBegin, "CombatBegin")
+    Command.Event.Detach(Event.Unit.Detail.Combat, CombatCheck, "CombatCheck")
+    Command.Event.Detach(Event.Combat.Death, CombatDeath, "CombatDeathCheck")
     Command.Event.Detach(Event.Unit.Detail.Ready, CombatEnd, "CombatEnd")
     Command.Event.Detach(Event.Buff.Add, getAddBuffName, "buffaddevent")
     Command.Event.Detach(Event.Buff.Remove, getRemoveBuffName, "buffremoveevent")
     Command.Event.Detach(Event.Unit.Castbar, getAbilityName, "AbilityName")
-    Command.Event.Detach(Event.Unit.Detail.Health, CheckHP, "CheckHP")
 end
 
 
 local function rra_start()
     rra_stop()
-    Command.Event.Attach(Event.Unit.Detail.Combat, CombatBegin, "CombatBegin")
+    Command.Event.Attach(Event.Unit.Detail.Combat, CombatCheck, "CombatCheck")
+    Command.Event.Attach(Event.Combat.Death, CombatDeath, "CombatDeathCheck")
     Command.Event.Attach(Event.Unit.Detail.Ready, CombatEnd, "CombatEnd")
     Command.Event.Attach(Event.Buff.Add, getAddBuffName, "buffaddevent")
     Command.Event.Attach(Event.Buff.Remove, getRemoveBuffName, "buffremoveevent")
     Command.Event.Attach(Event.Unit.Castbar, getAbilityName, "AbilityName")
-    Command.Event.Attach(Event.Unit.Detail.Combat, CombatBegin, "CombatBegin")
-    Command.Event.Attach(Event.Unit.Detail.Health, CheckHP, "CheckHP")
-    Command.Event.Attach(Event.Unit.Detail.Ready, CombatEnd, "CombatEnd")
+    print("Rift Raid Alert started")
+end
+
+
+local function start_check(addon)
+    print("/rra - for a list of commands")
+    if RiftRaidAlert_enabled then
+        rra_start()
+    end
 end
 
 
@@ -209,11 +165,12 @@ local function slashHandler(h, args)
         numargs = numargs + 1
     end
     if r[0] == "start" then
-        print("Rift Raid Alert started")
+        RiftRaidAlert_enabled = true
         rra_start()
         return
     end
     if r[0] == "stop" then
+        RiftRaidAlert_enabled = nil
         print("Rift Raid Alert stoped")
         rra_stop()
         return
@@ -222,8 +179,6 @@ local function slashHandler(h, args)
     print("/rra stop - Rift Raid Alert stop")
 end
 
-rra_bufflist = {}
-rra_combatbegin = "out_of_combat"
-rra_health = 0
-print("/rra - for a list of commands")
+
+Command.Event.Attach(Event.Addon.Startup.End, start_check, "StartCheck")
 Command.Event.Attach(Command.Slash.Register("rra"), slashHandler, "Command.Slash.Register")
