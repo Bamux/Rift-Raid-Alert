2e72d5a83dd663eeca87d53cce8f09df62f3e1c8
Bamux <bamueq@googlemail.com>
2016-07-12 22:52:31 +0200

    new features
    New Features:
    
    - added Fauxmire, Dark Genesis and Lord Fionn (not testet yet)
    - on a Ready Check Siri says the Name of Players which have no weaponstone, flask or food, with more than 4 players without this buffs Siri say only the number (you can disable this feature in the RiftRaidAlert.ini)
    - search the chat messages and alert you when certain phrases/keywords appear in the chat channel
    
    I added a folder „trigger“.  The triggers are now text files which you can easily edit. Triggers in the default.txt work anywhere. Triggers in the zonename.txt are only loaded when you're in this zone.

--- RiftRaidAlert.lua
+++ RiftRaidAlert.lua
@@ -30,16 +30,16 @@ local function CombatDeath(event, units)
     if rra_boss_id then
         local unit_details = Inspect.Unit.Detail(units)
         for id, detail in pairs(unit_details) do
-            if detail.id == rra_boss_id then
+            if detail.id == rra_boss_id and detail.health < 1 then
                 print("Combat End -> ".. detail.name)
                 local count = #rra_bufflist
                 for i=0, count do rra_bufflist[i]=nil end
                 rra_boss_id = nil
-                break
             end
         end
     end
 end
+
 
 -- print Debuffs on all Player's (Player <-  NPC) and Buffs on all NPC's (NPC <- Buff)
 local function getAddBuffName(event, unit, buffs)
@@ -66,6 +66,170 @@ local function getAddBuffName(event, unit, buffs)
                     local bufflist = { id = buffid, name = buff.name }
                     table.insert(rra_bufflist, bufflist)
                 end
+            end
+        end
+    end
+end
+
+
+local function rra_raidbuffcheck()
+    print("Buff Check")
+    local weaponstones_flasks_food = {}
+    local weaponstones_flasks = {}
+    local weaponstones_food = {}
+    local flasks_food = {}
+    local weaponstones = {}
+    local flasks = {}
+    local foods = {}
+    local count = 0
+    local groupmember = ""
+
+    for i=1, 20 do
+        local flask = false
+        local weaponstone = false
+        local food = false
+        local groupmember = string.format("group%02d", i)
+        local player = Inspect.Unit.Detail(groupmember)
+        local buffs = Inspect.Buff.List(groupmember)
+        if buffs and player.role ~= "tank" then
+            for buffid, typeid in pairs(buffs) do
+                local detail = Inspect.Buff.Detail(groupmember, buffid)
+                if detail and player then
+                    if detail.rune then
+                        if player.calling == "mage" or player.calling == "cleric" then
+                            if detail.rune == "r54A72A7D7486A939" then -- Pelagic Powerstone = r54A72A7D7486A939
+                                if detail.duration > 300 then
+                                    weaponstone = true
+                                end
+                            end
+                        else
+                            if detail.rune == "r762BD1423747FA3F" or detail.rune == "r0C2445386997ECE6" then -- Coral Oilstone = r762BD1423747FA3F, Coral Whetstone = r0C2445386997ECE6
+                                if detail.duration > 300 then
+                                    weaponstone = true
+                                end
+                            end
+                        end
+                    end
+                    if detail.type then
+                        if player.calling == "mage" or player.calling == "cleric" then
+                            if detail.type == "B5FB47C7B1CE019F8" or detail.type == "B2A1357781A34EE07" then --  Illustrious Brightsurge Vial = B5FB47C7B1CE019F8, Phenomenal Brightsurge Vial = B2A1357781A34EE07
+                                if detail.duration > 300 then
+                                    flask = true
+                                end
+                            end
+                            if detail.type == "B3CA755DE889572AE" then --  Feast of the Rhenke (SP) = B3CA755DE889572AE
+                                if detail.duration > 300 then
+                                    food = true
+                                end
+                            end
+                        else
+                            if detail.type == "B45441A66942B2875" or detail.type == "B4E57169B18162520" then --  Illustrious Powersurge Vial = B45441A66942B2875, Phenomenal Powersurge Vial = B4E57169B18162520
+                                if detail.duration > 300 then
+                                    flask = true
+                                end
+                            end
+                            if detail.type == "B3CA755E06191712E" then --  Feast of the Rhenke (AP) = B3CA755E06191712E
+                                if detail.duration > 300 then
+                                    food = true
+                                end
+                            end
+                        end
+                    end
+                end
+            end
+            if weaponstone == false and flask == false  and food == false then
+                table.insert(weaponstones_flasks_food, player.name)
+                count = count + 1
+            elseif weaponstone == false and flask == false then
+                table.insert(weaponstones_flasks, player.name)
+                count = count + 1
+            elseif weaponstone == false and food == false then
+                table.insert(weaponstones_food, player.name)
+                count = count + 1
+            elseif flask == false and food == false then
+                table.insert(flasks_food, player.name)
+                count = count + 1
+            elseif weaponstone == false then
+                table.insert(weaponstones, player.name)
+                count = count + 1
+            elseif flask == false then
+                table.insert(flasks, player.name)
+                count = count + 1
+            elseif food == false then
+                table.insert(foods, player.name)
+                count = count + 1
+            end
+        end
+    end
+
+    if count > 0 and count <= 4 then
+        local buff_count = #weaponstones_flasks_food
+        if buff_count > 0 then
+            for i=1, buff_count do
+                print("Raidbuff missing -> " .. weaponstones_flasks_food[i])
+            end
+            print("Raidbuff missing <--- Weaponstone, Flask and Food")
+        end
+        buff_count = #weaponstones_flasks
+        if buff_count > 0 then
+            for i=1, buff_count do
+                print("Raidbuff missing -> " .. weaponstones_flasks[i])
+            end
+            print("Raidbuff missing <-- Weaponstone and Flask")
+        end
+        buff_count = #weaponstones_food
+        if buff_count > 0 then
+            for i=1, buff_count do
+                print("Raidbuff missing -> " .. weaponstones_food[i])
+            end
+            print("Raidbuff missing <-- Weaponstone and Food")
+        end
+        buff_count = #flasks_food
+        if buff_count > 0 then
+            for i=1, buff_count do
+                print("Raidbuff missing -> " .. flasks_food[i])
+            end
+            print("Raidbuff missing <-- Flask and Food")
+        end
+        buff_count = #weaponstones
+        if buff_count > 0 then
+            for i=1, buff_count do
+                print("Raidbuff missing -> " .. weaponstones[i])
+            end
+            print("Raidbuff missing <- Weaponstone")
+        end
+        local buff_count = #flasks
+        if buff_count > 0 then
+            for i=1, buff_count do
+                print("Raidbuff missing -> " .. flasks[i])
+            end
+            print("Raidbuff missing <- Flask")
+        end
+        local buff_count = #foods
+        if buff_count > 0 then
+            for i=1, buff_count do
+                print("Raidbuff missing -> " .. foods[i])
+            end
+            print("Raidbuff missing <- Food")
+        end
+    elseif count > 4 then
+        print("Raidbuff missing -> " .. count)
+        print("Raidbuff missing number <- Weaponstone, Flask or Food")
+    end
+end
+
+
+local function test()
+    local buffs = Inspect.Buff.List("player.target")
+    for buffid, typeid in pairs(buffs) do
+        local detail = Inspect.Buff.Detail("player.target", buffid)
+        --dump (detail)
+        if detail then
+            if detail.rune then
+                print(detail.name .. " = " .. detail.rune)
+            end
+            if detail.type then
+                print(detail.name .. " = " .. detail.type)
             end
         end
     end
@@ -127,13 +291,41 @@ local function getAbilityName(event, units)
 end
 
 
+local function Zone()
+    local detail = Inspect.Unit.Detail("player")
+    if detail then
+        local zone_id = detail.zone
+        local zone = Inspect.Zone.Detail(zone_id)
+        if zone.name then
+            print("Rift Raid Alert Trigger -> ".. zone.name)
+        end
+    end
+end
+
+
+local function ReadyCheck(event, units)
+    local unit_details = Inspect.Unit.Detail(units)
+    local player = Inspect.Unit.Detail("player")
+    for id, detail in pairs(unit_details) do
+        if detail.id == player.id then
+            if detail.ready then
+                CombatEnd()
+                Zone()
+                rra_raidbuffcheck()
+            end
+        end
+    end
+end
+
+
 local function rra_stop()
     Command.Event.Detach(Event.Unit.Detail.Combat, CombatCheck, "CombatCheck")
     Command.Event.Detach(Event.Combat.Death, CombatDeath, "CombatDeathCheck")
-    Command.Event.Detach(Event.Unit.Detail.Ready, CombatEnd, "CombatEnd")
+    Command.Event.Detach(Event.Unit.Detail.Ready, ReadyCheck, "ReadyCheck")
     Command.Event.Detach(Event.Buff.Add, getAddBuffName, "buffaddevent")
     Command.Event.Detach(Event.Buff.Remove, getRemoveBuffName, "buffremoveevent")
     Command.Event.Detach(Event.Unit.Castbar, getAbilityName, "AbilityName")
+    --Command.Event.Detach(Event.Unit.Detail.Zone, ChangeZone, "ChangeZone")
 end
 
 
@@ -141,10 +333,11 @@ local function rra_start()
     rra_stop()
     Command.Event.Attach(Event.Unit.Detail.Combat, CombatCheck, "CombatCheck")
     Command.Event.Attach(Event.Combat.Death, CombatDeath, "CombatDeathCheck")
-    Command.Event.Attach(Event.Unit.Detail.Ready, CombatEnd, "CombatEnd")
+    Command.Event.Attach(Event.Unit.Detail.Ready, ReadyCheck, "ReadyCheck")
     Command.Event.Attach(Event.Buff.Add, getAddBuffName, "buffaddevent")
     Command.Event.Attach(Event.Buff.Remove, getRemoveBuffName, "buffremoveevent")
     Command.Event.Attach(Event.Unit.Castbar, getAbilityName, "AbilityName")
+    --Command.Event.Attach(Event.Unit.Detail.Zone, ChangeZone, "ChangeZone")
     print("Rift Raid Alert started")
 end
 
@@ -152,7 +345,11 @@ end
 local function start_check(addon)
     print("/rra - for a list of commands")
     if RiftRaidAlert_enabled then
-        rra_start()
+        if RiftRaidAlert_enabled == "start" then
+            rra_start()
+        elseif RiftRaidAlert_enabled == "keywords" then
+            print("Rift Raid Alert Trigger -> keywords")
+        end
     end
 end
 
@@ -164,21 +361,47 @@ local function slashHandler(h, args)
         r[numargs] = token
         numargs = numargs + 1
     end
-    if r[0] == "start" then
-        RiftRaidAlert_enabled = true
-        rra_start()
+    if r[0] == "stop" then
+        if RiftRaidAlert_enabled == "keywords" then
+            print("Rift Raid Alert Trigger <- keywords off")
+        else
+           print("Rift Raid Alert stoped")
+        end
+        RiftRaidAlert_enabled = nil
+        rra_stop()
         return
     end
-    if r[0] == "stop" then
-        RiftRaidAlert_enabled = nil
-        print("Rift Raid Alert stoped")
+    if r[0] == "start" then
+        if RiftRaidAlert_enabled == "keywords" then
+            print("Rift Raid Alert Trigger <- keywords off")
+        end
+        RiftRaidAlert_enabled = "start"
+        rra_start()
+        Zone()
+        return
+    end
+    if r[0] == "keywords" then
+        if RiftRaidAlert_enabled == "start" then
+            print("Rift Raid Alert stoped")
+        end
+        RiftRaidAlert_enabled = "keywords"
+        print("Rift Raid Alert Trigger -> Keywords on")
         rra_stop()
+        return
+    end
+    if r[0] == "check" then
+        rra_raidbuffcheck()
+        return
+    end
+    if r[0] == "target" then
+        print("Target Check")
+        test()
         return
     end
     print("/rra start - Rift Raid Alert start")
     print("/rra stop - Rift Raid Alert stop")
+    print("/rra keywords - search the chat for keywords")
 end
-
 
 Command.Event.Attach(Event.Addon.Startup.End, start_check, "StartCheck")
 Command.Event.Attach(Command.Slash.Register("rra"), slashHandler, "Command.Slash.Register")
